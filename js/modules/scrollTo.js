fde67bff793b8a7fb4bc1eb35e11c2189efcc322
BlackjackDaniels54 <polovinchukevgeniy2@gmail.com>
2023-10-11 14:52:00 +0300

    11.10

--- js/modules/scrollTo.js
+++ js/modules/scrollTo.js
@@ -1,18 +1,20 @@
 
 export function ScrollTo(){
-    const link = document.querySelector('.home_news');
-          
-    link.addEventListener('click', (e) => {
+    const links = document.querySelectorAll('.home_news');
+       
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
         
-        const gotoBlock = document.querySelector(e.target.dataset.goto);
-        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - 90; 
-        
-        window.scrollTo({
-            top: gotoBlockValue,
-            behavior:"smooth"
-        })
-        
-        document.querySelector('.nav-menu-wrapper').classList.remove('show');
-})
+            const gotoBlock = document.querySelector(e.target.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - 90; 
+            
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior:"smooth"
+            })
+            
+            document.querySelector('.nav-menu-wrapper').classList.remove('show');
+    })
+    })
 }
     
