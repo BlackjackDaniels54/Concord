2195ae28531fc0d0da5b5f9f38d5879504f75fe6
Evheniy Polovinchuk <86078300+BlackjackDaniels54@users.noreply.github.com>
2023-10-21 21:49:41 +0300

    faces

--- js/lawyers.js
+++ js/lawyers.js
@@ -6,19 +6,23 @@ body.innerHTML = null;
 
 data.forEach((lawyer, index) => {
     const div = document.createElement('div');
-    div.classList.add('col-sm-6', 'col-lg-4', 'mb-5', 'employee');
+    div.classList.add('col-sm-6', 'col-lg-4', 'col-xl-3',  'mb-5', 'employee');
     div.setAttribute('data-id', index);
     div.innerHTML = `
     
         <div class="lawyer-card_container">
             <div class="lawyer-card_content">
-                <div class="mb-4 img-lawyer-card_container">
+                <div class="mb-3 img-lawyer-card_container">
                     <img src="../img/lawyers/${lawyer.image}" alt="">
                 </div>
-                <div class="lawyer-info_container mb-3">
+                <div class="lawyer-info_container">
                     <div class="lawyer-name">${lawyer.name + ' ' + lawyer.surname}</div>
                     <div class="lawyer-city">${lawyer.city}</div>
-                    <div class="lawyer-email">${lawyer.email}</div>
+                    
+                    <a class="lawyer-email" href="mailto:${lawyer.email}">
+                        ${lawyer.email}
+                    </a>
+                    
                 </div>
             </div>
         </div>
